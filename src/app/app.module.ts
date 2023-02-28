a4055740d5ff400951e608fa9b6c23ecd6fb1b5b
sermarmar <sermarmar1995@gmail.com>
2023-02-28 15:51:19 +0100

    Ejercicio contador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contandor.component';
+import { ContadorComponent } from './contador/contandor.component';
 
 @NgModule({
   declarations: [
